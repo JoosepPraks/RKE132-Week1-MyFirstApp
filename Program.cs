a3a0d25e6e1fe9d0c44c493830266689959b9dab
JoosepPraks <joosep.praks@tktk.ee>
2024-04-08 18:28:03 +0300

    create new app

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//string - sõne
+string userName = Console.ReadLine();
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!");
